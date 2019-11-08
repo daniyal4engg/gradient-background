f3f1161a34661c89bce556226227f96851476c2b
Daniyal Shaikh <daniyal4engg@gmail.com>
2019-11-09 02:32:18 +0530

    stateged and commit

--- color gradient/script.js
+++ color gradient/script.js
@@ -1,7 +1,7 @@
-var css = document.querySelector("h3");
+var css = document.getElementById("paint");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
-var body = document.getElementById("container");
+var body = document.getElementsByTagName("body")[0];
 
 function colorpicker() {
   body.style.background =
